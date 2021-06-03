ae677d02d1b70a2dd9ad9397c7bea973e3a301d6
vipul022 <vipul.seth022@gmail.com>
2021-06-03 15:22:04 +1000

    added an alternate solution to the two-fer challenge

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -3,11 +3,15 @@
 // convenience to get you started writing code faster.
 //
 
-export const twoFer = (name) => {
-  let result = "One for you, one for me.";
-  if (name) {
-    return (result = result.replace(/you/i, name));
-  } else {
-    return result;
-  }
+// export const twoFer = (name) => {
+//   let result = "One for you, one for me.";
+//   if (name) {
+//     return (result = result.replace(/you/i, name));
+//   } else {
+//     return result;
+//   }
+// };
+
+export const twoFer = (name = "") => {
+  return name ? `One for ${name}, one for me.` : `One for you, one for me.`;
 };
