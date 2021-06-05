565f0fc155496992797f8c73d442ac20bb1ae902
vipul022 <vipul.seth022@gmail.com>
2021-06-05 15:26:41 +1000

    competed more challenges and added instructions for all challenges

---
+++ javascript/reverse-string/reverse-string.js
@@ -0,0 +1,8 @@
+// Reverse a string
+
+// For example: input: "cool" output: "looc"
+
+
+export const reverseString = (string) => string.split("").reverse().join("");
+
+
--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,16 +1,22 @@
-//
-// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+// Two-fer or 2-fer is short for two for one. One for you and one for me.
 
-// export const twoFer = (name) => {
-//   let result = "One for you, one for me.";
-//   if (name) {
-//     return (result = result.replace(/you/i, name));
-//   } else {
-//     return result;
-//   }
-// };
+// Given a name, return a string with the message:
+
+// One for name, one for me.
+// Where "name" is the given name.
+
+// However, if the name is missing, return the string:
+
+// One for you, one for me.
+// Here are some examples:
+
+// Name	String to return
+// Alice	One for Alice, one for me.
+// Bob	One for Bob, one for me.
+// One for you, one for me.
+// Zaphod	One for Zaphod, one for me.
+
+
 
 export const twoFer = (name = "you") => {
   return `One for ${name}, one for me.`;
